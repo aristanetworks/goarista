b644779d6bf49c7074367d28b87ad80459cd292c
Fabrice Rabaute <fabrice@arista.com>
2014-11-19 17:38:21 -0800

    Add support for byte slices.
    Change-Id: Ic842fef3621619835cb87be761e547c6e1541bcf

--- test/deepequal.go
+++ test/deepequal.go
@@ -99,8 +99,8 @@ func DeepEqual(a, b interface{}) bool {
 			}
 		}
 		return true
-	case []uint32:
-		v, ok := b.([]uint32)
+	case []byte:
+		v, ok := b.([]byte)
 		if !ok || len(a) != len(v) {
 			return false
 		}
@@ -112,6 +112,17 @@ func DeepEqual(a, b interface{}) bool {
 		return true
 	case []uint16:
 		v, ok := b.([]uint16)
+		if !ok || len(a) != len(v) {
+			return false
+		}
+		for i, s := range a {
+			if s != v[i] {
+				return false
+			}
+		}
+		return true
+	case []uint32:
+		v, ok := b.([]uint32)
 		if !ok || len(a) != len(v) {
 			return false
 		}
