3d45395ed19976790cde0299fb8f316f6ca998b5
Fabrice Rabaute <fabrice@arista.com>
2015-03-23 11:23:16 -0700

    Refactor the Key in notif updates and deletes.
    Deletes need to be changed from []string to []interface{}
    Instead of using interface{}, we've decided to use a wrapped object
    as the Key. This Key object is also used in the update map.
    
    Change-Id: I364bc5aa6427e53bb35647cdd8aa3d9457d8c26d
    Signed-off-by: Benoit Sigoure <tsuna@arista.com>

--- types/key.go
+++ types/key.go
@@ -3,11 +3,28 @@
 
 package types
 
-// Key represents the Key in the updates and deletes of the Notification objects
+import (
+	"fmt"
+)
+
+// Key represents the Key in the updates and deletes of the Notification
+// objects.  The only reason this exists is that Go won't let us define
+// our own hash function for non-hashable types, and unfortunately we
+// need to be able to index maps by map[string]interface{} objects.
 type Key interface {
 	Key() interface{}
 	String() string
-	Equal(other Key) bool
+	Equal(other interface{}) bool
+
+	// Helper methods to manipulate maps keyed by `Key'.
+
+	// GetFromMap returns the value for the entry of this Key.
+	GetFromMap(map[Key]interface{}) (interface{}, bool)
+	// DeleteFromMap deletes the entry in the map for this Key.
+	// This is a no-op if the key does not exist in the map.
+	DeleteFromMap(map[Key]interface{})
+	// SetToMap updates or inserts an entry in the map for this Key.
+	SetToMap(m map[Key]interface{}, value interface{})
 }
 
 type keyImpl struct {
@@ -17,33 +34,109 @@ type keyImpl struct {
 // NewKey returns a Key object implementing the Key interface
 // to wrapped the key passed in
 func NewKey(intf interface{}) Key {
+	switch t := intf.(type) {
+	case map[string]interface{}:
+		intf = &t
+	case int8, int16, int32, int64,
+		uint8, uint16, uint32, uint64,
+		float32, float64, string, bool:
+	default:
+		panic(fmt.Sprintf("Invalid type for key: %T", intf))
+	}
 	return keyImpl{key: intf}
 }
 
 func (k keyImpl) Key() interface{} {
+	if m, ok := k.key.(*map[string]interface{}); ok {
+		return *m
+	}
 	return k.key
 }
 
 func (k keyImpl) String() string {
-	s, err := StringifyInterface(k.key)
+	str, err := StringifyInterface(k.key)
 	if err != nil {
-		panic("Unable to stringify a key: " + err.Error())
+		panic("Unable to stringify Key: " + err.Error())
 	}
-	return s
+	return str
 }
 
-func (k keyImpl) Equal(other Key) bool {
-	return keyEqual(k.key, other.Key())
+func isHashableMap(m map[Key]interface{}) bool {
+	for k := range m {
+		return k.(keyImpl).isHashable()
+	}
+	return false
+}
+func (k keyImpl) isHashable() bool {
+	_, ok := k.key.(*map[string]interface{})
+	return !ok
+}
+
+func (k keyImpl) GetFromMap(m map[Key]interface{}) (interface{}, bool) {
+	if len(m) == 0 {
+		return nil, false
+	}
+	if isHashableMap(m) {
+		v, ok := m[k]
+		return v, ok
+	}
+	for key, value := range m {
+		if k.Equal(key) {
+			return value, true
+		}
+	}
+	return nil, false
+}
+func (k keyImpl) DeleteFromMap(m map[Key]interface{}) {
+	if len(m) == 0 {
+		return
+	}
+	if isHashableMap(m) {
+		delete(m, k)
+		return
+	}
+	for key := range m {
+		if k.Equal(key) {
+			delete(m, key)
+			return
+		}
+	}
+}
+
+func (k keyImpl) SetToMap(m map[Key]interface{}, value interface{}) {
+	if isHashableMap(m) {
+		m[k] = value
+		return
+	}
+	for key := range m {
+		if k.Equal(key) {
+			m[key] = value
+			return
+		}
+	}
+	m[k] = value
+}
+
+func (k keyImpl) Equal(other interface{}) bool {
+	o, ok := other.(Key)
+	if !ok {
+		return false
+	}
+	if m, ok := k.key.(*map[string]interface{}); ok {
+		m2, ok := o.Key().(map[string]interface{})
+		return ok && keyEqual(*m, m2)
+	}
+	return keyEqual(k.key, o.Key())
 }
 
 func keyEqual(a, b interface{}) bool {
-	if a, ok := a.(*map[string]interface{}); ok {
-		b, ok := b.(*map[string]interface{})
-		if !ok || len(*a) != len(*b) {
+	if a, ok := a.(map[string]interface{}); ok {
+		b, ok := b.(map[string]interface{})
+		if !ok || len(a) != len(b) {
 			return false
 		}
-		for k, av := range *a {
-			if bv, ok := (*b)[k]; !ok || !keyEqual(av, bv) {
+		for k, av := range a {
+			if bv, ok := (b)[k]; !ok || !keyEqual(av, bv) {
 				return false
 			}
 		}
--- types/key_test.go
+++ types/key_test.go
@@ -1,12 +1,13 @@
 // Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
-package types_test
+package types
 
 import (
+	"fmt"
 	"testing"
 
-	. "github.com/aristanetworks/goarista/types"
+	"github.com/aristanetworks/goarista/test"
 )
 
 func TestKeyEqual(t *testing.T) {
@@ -15,14 +16,6 @@ func TestKeyEqual(t *testing.T) {
 		b      Key
 		result bool
 	}{{
-		a:      NewKey(3),
-		b:      NewKey(3),
-		result: true,
-	}, {
-		a:      NewKey(3),
-		b:      NewKey(4),
-		result: false,
-	}, {
 		a:      NewKey("foo"),
 		b:      NewKey("foo"),
 		result: true,
@@ -31,28 +24,28 @@ func TestKeyEqual(t *testing.T) {
 		b:      NewKey("bar"),
 		result: false,
 	}, {
-		a:      NewKey(&map[string]interface{}{}),
+		a:      NewKey(map[string]interface{}{}),
 		b:      NewKey("bar"),
 		result: false,
 	}, {
-		a:      NewKey(&map[string]interface{}{}),
-		b:      NewKey(&map[string]interface{}{}),
+		a:      NewKey(map[string]interface{}{}),
+		b:      NewKey(map[string]interface{}{}),
 		result: true,
 	}, {
-		a:      NewKey(&map[string]interface{}{"a": 3}),
-		b:      NewKey(&map[string]interface{}{}),
+		a:      NewKey(map[string]interface{}{"a": 3}),
+		b:      NewKey(map[string]interface{}{}),
 		result: false,
 	}, {
-		a:      NewKey(&map[string]interface{}{"a": 3}),
-		b:      NewKey(&map[string]interface{}{"b": 4}),
+		a:      NewKey(map[string]interface{}{"a": 3}),
+		b:      NewKey(map[string]interface{}{"b": 4}),
 		result: false,
 	}, {
-		a:      NewKey(&map[string]interface{}{"a": 3}),
-		b:      NewKey(&map[string]interface{}{"a": 4}),
+		a:      NewKey(map[string]interface{}{"a": 3}),
+		b:      NewKey(map[string]interface{}{"a": 4}),
 		result: false,
 	}, {
-		a:      NewKey(&map[string]interface{}{"a": 3}),
-		b:      NewKey(&map[string]interface{}{"a": 3}),
+		a:      NewKey(map[string]interface{}{"a": 3}),
+		b:      NewKey(map[string]interface{}{"a": 3}),
 		result: true,
 	}}
 
@@ -62,6 +55,354 @@ func TestKeyEqual(t *testing.T) {
 				tcase.a,
 				tcase.b,
 				tcase.result)
+		}
+	}
+
+	if NewKey("a").Equal(32) == true {
+		t.Error("Wrong result for different types case")
+	}
+}
+
+func TestIsHashable(t *testing.T) {
+	tests := []struct {
+		k interface{}
+		h bool
+	}{{
+		true,
+		true,
+	}, {
+		false,
+		true,
+	}, {
+		uint8(3),
+		true,
+	}, {
+		uint16(3),
+		true,
+	}, {
+		uint32(3),
+		true,
+	}, {
+		uint64(3),
+		true,
+	}, {
+		int8(3),
+		true,
+	}, {
+		int16(3),
+		true,
+	}, {
+		int32(3),
+		true,
+	}, {
+		int64(3),
+		true,
+	}, {
+		float32(3.2),
+		true,
+	}, {
+		float64(3.3),
+		true,
+	}, {
+		"foobar",
+		true,
+	}, {
+		map[string]interface{}{"foo": "bar"},
+		false,
+	}}
+
+	for _, tcase := range tests {
+		if NewKey(tcase.k).(keyImpl).isHashable() != tcase.h {
+			t.Errorf("Wrong result for case:\nk: %#v",
+				tcase.k)
+
+		}
+	}
+}
+
+func TestGetFromMap(t *testing.T) {
+	tests := []struct {
+		k     Key
+		m     map[Key]interface{}
+		v     interface{}
+		found bool
+	}{{
+		k:     NewKey("a"),
+		m:     map[Key]interface{}{NewKey("a"): "b"},
+		v:     "b",
+		found: true,
+	}, {
+		k:     NewKey(uint32(35)),
+		m:     map[Key]interface{}{NewKey(uint32(35)): "c"},
+		v:     "c",
+		found: true,
+	}, {
+		k:     NewKey(uint32(37)),
+		m:     map[Key]interface{}{NewKey(uint32(36)): "c"},
+		found: false,
+	}, {
+		k:     NewKey(uint32(37)),
+		m:     map[Key]interface{}{},
+		found: false,
+	}, {
+		k: NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}),
+		m: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+		},
+		v:     "foo",
+		found: true,
+	}}
+
+	for _, tcase := range tests {
+		v, ok := tcase.k.GetFromMap(tcase.m)
+		if tcase.found != ok {
+			t.Errorf("Wrong retrieval result for case:\nk: %#v\nm: %#v\nv: %#v",
+				tcase.k,
+				tcase.m,
+				tcase.v)
+		} else if tcase.found && !ok {
+			t.Errorf("Unable to retrieve value for case:\nk: %#v\nm: %#v\nv: %#v",
+				tcase.k,
+				tcase.m,
+				tcase.v)
+		} else if tcase.found && !test.DeepEqual(tcase.v, v) {
+			t.Errorf("Wrong result for case:\nk: %#v\nm: %#v\nv: %#v",
+				tcase.k,
+				tcase.m,
+				tcase.v)
+		}
+	}
+}
+
+func TestDeleteFromMap(t *testing.T) {
+	tests := []struct {
+		k Key
+		m map[Key]interface{}
+		r map[Key]interface{}
+	}{{
+		k: NewKey("a"),
+		m: map[Key]interface{}{NewKey("a"): "b"},
+		r: map[Key]interface{}{},
+	}, {
+		k: NewKey("b"),
+		m: map[Key]interface{}{NewKey("a"): "b"},
+		r: map[Key]interface{}{NewKey("a"): "b"},
+	}, {
+		k: NewKey("a"),
+		m: map[Key]interface{}{},
+		r: map[Key]interface{}{},
+	}, {
+		k: NewKey(uint32(35)),
+		m: map[Key]interface{}{NewKey(uint32(35)): "c"},
+		r: map[Key]interface{}{},
+	}, {
+		k: NewKey(uint32(36)),
+		m: map[Key]interface{}{NewKey(uint32(35)): "c"},
+		r: map[Key]interface{}{NewKey(uint32(35)): "c"},
+	}, {
+		k: NewKey(uint32(37)),
+		m: map[Key]interface{}{NewKey(uint32(36)): "c"},
+		r: map[Key]interface{}{NewKey(uint32(36)): "c"},
+	}, {
+		k: NewKey(uint32(37)),
+		m: map[Key]interface{}{},
+		r: map[Key]interface{}{},
+	}, {
+		k: NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}),
+		m: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+		},
+		r: map[Key]interface{}{},
+	}}
+
+	for _, tcase := range tests {
+		tcase.k.DeleteFromMap(tcase.m)
+		if !test.DeepEqual(tcase.m, tcase.r) {
+			t.Errorf("Wrong result for case:\nk: %#v\nm: %#v\nr: %#v",
+				tcase.k,
+				tcase.m,
+				tcase.r)
+		}
+	}
+}
+
+func TestSetToMap(t *testing.T) {
+	tests := []struct {
+		k Key
+		v interface{}
+		m map[Key]interface{}
+		r map[Key]interface{}
+	}{{
+		k: NewKey("a"),
+		v: "c",
+		m: map[Key]interface{}{NewKey("a"): "b"},
+		r: map[Key]interface{}{NewKey("a"): "c"},
+	}, {
+		k: NewKey("b"),
+		v: uint64(56),
+		m: map[Key]interface{}{NewKey("a"): "b"},
+		r: map[Key]interface{}{
+			NewKey("a"): "b",
+			NewKey("b"): uint64(56),
+		},
+	}, {
+		k: NewKey("a"),
+		v: "foo",
+		m: map[Key]interface{}{},
+		r: map[Key]interface{}{NewKey("a"): "foo"},
+	}, {
+		k: NewKey(uint32(35)),
+		v: "d",
+		m: map[Key]interface{}{NewKey(uint32(35)): "c"},
+		r: map[Key]interface{}{NewKey(uint32(35)): "d"},
+	}, {
+		k: NewKey(uint32(36)),
+		v: true,
+		m: map[Key]interface{}{NewKey(uint32(35)): "c"},
+		r: map[Key]interface{}{
+			NewKey(uint32(35)): "c",
+			NewKey(uint32(36)): true,
+		},
+	}, {
+		k: NewKey(uint32(37)),
+		v: false,
+		m: map[Key]interface{}{NewKey(uint32(36)): "c"},
+		r: map[Key]interface{}{
+			NewKey(uint32(36)): "c",
+			NewKey(uint32(37)): false,
+		},
+	}, {
+		k: NewKey(uint32(37)),
+		v: "foobar",
+		m: map[Key]interface{}{},
+		r: map[Key]interface{}{NewKey(uint32(37)): "foobar"},
+	}, {
+		k: NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}),
+		v: "foobar",
+		m: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+		},
+		r: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foobar",
+		},
+	}, {
+		k: NewKey(map[string]interface{}{"a": "b", "c": uint64(7)}),
+		v: "foobar",
+		m: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+		},
+		r: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(7)}): "foobar",
+		},
+	}, {
+		k: NewKey(map[string]interface{}{"a": "b", "d": uint64(6)}),
+		v: "barfoo",
+		m: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+		},
+		r: map[Key]interface{}{
+			NewKey(map[string]interface{}{"a": "b", "c": uint64(4)}): "foo",
+			NewKey(map[string]interface{}{"a": "b", "d": uint64(6)}): "barfoo",
+		},
+	}}
+
+	for i, tcase := range tests {
+		tcase.k.SetToMap(tcase.m, tcase.v)
+		if !test.DeepEqual(tcase.m, tcase.r) {
+			t.Errorf("Wrong result for case %d:\nk: %#v\nm: %#v\nr: %#v",
+				i,
+				tcase.k,
+				tcase.m,
+				tcase.r)
+		}
+	}
+}
+
+func BenchmarkSetToMapWithStringKey(b *testing.B) {
+	m := map[Key]interface{}{
+		NewKey("a"):   true,
+		NewKey("a1"):  true,
+		NewKey("a2"):  true,
+		NewKey("a3"):  true,
+		NewKey("a4"):  true,
+		NewKey("a5"):  true,
+		NewKey("a6"):  true,
+		NewKey("a7"):  true,
+		NewKey("a8"):  true,
+		NewKey("a9"):  true,
+		NewKey("a10"): true,
+		NewKey("a11"): true,
+		NewKey("a12"): true,
+		NewKey("a13"): true,
+		NewKey("a14"): true,
+		NewKey("a15"): true,
+		NewKey("a16"): true,
+		NewKey("a17"): true,
+		NewKey("a18"): true,
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		NewKey(fmt.Sprintf("b%d", i)).SetToMap(m, true)
+	}
+}
+
+func BenchmarkSetToMapWithUint64Key(b *testing.B) {
+	m := map[Key]interface{}{
+		NewKey(uint64(1)):  true,
+		NewKey(uint64(2)):  true,
+		NewKey(uint64(3)):  true,
+		NewKey(uint64(4)):  true,
+		NewKey(uint64(5)):  true,
+		NewKey(uint64(6)):  true,
+		NewKey(uint64(7)):  true,
+		NewKey(uint64(8)):  true,
+		NewKey(uint64(9)):  true,
+		NewKey(uint64(10)): true,
+		NewKey(uint64(11)): true,
+		NewKey(uint64(12)): true,
+		NewKey(uint64(13)): true,
+		NewKey(uint64(14)): true,
+		NewKey(uint64(15)): true,
+		NewKey(uint64(16)): true,
+		NewKey(uint64(17)): true,
+		NewKey(uint64(18)): true,
+		NewKey(uint64(19)): true,
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		NewKey(uint64(i)).SetToMap(m, true)
+	}
+}
+
+func BenchmarkGetFromMapWithMapKey(b *testing.B) {
+	m := map[Key]interface{}{
+		NewKey(map[string]interface{}{"a0": true}):  true,
+		NewKey(map[string]interface{}{"a1": true}):  true,
+		NewKey(map[string]interface{}{"a2": true}):  true,
+		NewKey(map[string]interface{}{"a3": true}):  true,
+		NewKey(map[string]interface{}{"a4": true}):  true,
+		NewKey(map[string]interface{}{"a5": true}):  true,
+		NewKey(map[string]interface{}{"a6": true}):  true,
+		NewKey(map[string]interface{}{"a7": true}):  true,
+		NewKey(map[string]interface{}{"a8": true}):  true,
+		NewKey(map[string]interface{}{"a9": true}):  true,
+		NewKey(map[string]interface{}{"a10": true}): true,
+		NewKey(map[string]interface{}{"a11": true}): true,
+		NewKey(map[string]interface{}{"a12": true}): true,
+		NewKey(map[string]interface{}{"a13": true}): true,
+		NewKey(map[string]interface{}{"a14": true}): true,
+		NewKey(map[string]interface{}{"a15": true}): true,
+		NewKey(map[string]interface{}{"a16": true}): true,
+		NewKey(map[string]interface{}{"a17": true}): true,
+		NewKey(map[string]interface{}{"a18": true}): true,
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := NewKey(map[string]interface{}{fmt.Sprintf("a%d", i%19): true})
+		_, found := key.GetFromMap(m)
+		if !found {
+			b.Fatalf("WTF: %#v", key)
 		}
 	}
 }
