7a5a9c4f7c4e051683d873c7dac480d85823aa6a
Benoit Sigoure <tsuna@arista.com>
2015-06-17 16:46:59 -0700

    test: Better pretty-representation of byte array.
    Reminder: in Go the `byte' type and the `uint8' type are the same thing.
    So here whenever we see a `[]uint8', we assume it's a `[]byte' and print
    it in a more readable manner.
    
    Change-Id: I58b0b756330fafaa0dca62c4d5b7aec55780ae20

--- test/pretty.go
+++ test/pretty.go
@@ -124,6 +124,13 @@ func prettyPrintWithType(v reflect.Value, done ptrSet, depth int, showType bool)
 	case reflect.Array, reflect.Slice:
 		l := v.Len()
 		var r []byte
+		if v.Type().Elem().Kind() == reflect.Uint8 {
+			b := v.Interface().([]byte)
+			r = append(r, []byte(`[]byte(`)...)
+			r = append(r, []byte(fmt.Sprintf("%q", b))...)
+			r = append(r, ')')
+			return string(r)
+		}
 		r = append(r, []byte(v.Type().String())...)
 		r = append(r, '{')
 		for i := 0; i < l; i++ {
