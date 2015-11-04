232e02555e5c0b85164a0527887de2e797fbd8e4
Benoit Sigoure <tsunanet@gmail.com>
2015-11-03 17:38:39 -0800

    key: Adjust the license to ASLv2.

--- key/key_test.go
+++ key/key_test.go
@@ -1,5 +1,6 @@
-// Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
-// Arista Networks, Inc. Confidential and Proprietary.
+// Copyright (C) 2015  Arista Networks, Inc.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the COPYING file.
 
 package key_test
 
