77d868633bde8cd1e01cfe800bcd68620b7d70c2
Giuseppe Valente <gvalente@arista.com>
2016-04-11 16:19:28 -0700

    kafka: strip path in default topic
    Change-Id: Ic8e3497133314aa6a3c04d2b03f2592381d8a683

--- kafka/flag.go
+++ kafka/flag.go
@@ -7,10 +7,11 @@ package kafka
 import (
 	"flag"
 	"os"
+	"path/filepath"
 )
 
 // Addresses is the flag for kafka's comma-separated addresses
 var Addresses = flag.String("kafkaaddrs", "", "kafka's comma-separated addresses")
 
 // Topic is the flag for kafka's topic
-var Topic = flag.String("kafkatopic", os.Args[0], "kafka's topic")
+var Topic = flag.String("kafkatopic", filepath.Base(os.Args[0]), "kafka's topic")
