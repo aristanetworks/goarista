ee42c27ece9100039d83dbee9e9affd7d7ab058e
Giuseppe Valente <gvalente@arista.com>
2016-11-07 13:44:59 -0800

    openconfig: simplify json test
    Change-Id: I71940eb1413a96c94d8dc51352a65913cab2e23a

--- openconfig/json_test.go
+++ openconfig/json_test.go
@@ -22,84 +22,76 @@ func TestNotificationToMap(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	notification := openconfig.Notification{
-		Prefix: &openconfig.Path{
-			Element: []string{
-				"Smash", "routing", "pim", "sparsemode", "status", "default",
-			},
-		},
-		Delete: []*openconfig.Path{
-			&openconfig.Path{
+	tests := []struct {
+		notification openconfig.Notification
+		json         map[string]interface{}
+	}{{
+		notification: openconfig.Notification{
+			Prefix: &openconfig.Path{
 				Element: []string{
-					"route", "237.255.255.250_0.0.0.0",
-				}},
-			&openconfig.Path{
-				Element: []string{
-					"route", "238.255.255.250_0.0.0.0",
+					"foo", "bar",
 				},
 			},
-		},
-		Update: []*openconfig.Update{{
-			Path: &openconfig.Path{
-				Element: []string{
-					"route",
+			Delete: []*openconfig.Path{
+				&openconfig.Path{
+					Element: []string{
+						"route", "237.255.255.250_0.0.0.0",
+					}},
+				&openconfig.Path{
+					Element: []string{
+						"route", "238.255.255.250_0.0.0.0",
+					},
 				},
 			},
-			Value: &openconfig.Value{
-				Value: valueJSON,
+			Update: []*openconfig.Update{{
+				Path: &openconfig.Path{
+					Element: []string{
+						"route",
+					},
+				},
+				Value: &openconfig.Value{
+					Value: valueJSON,
+				},
+			}},
+		},
+		json: map[string]interface{}{
+			"timestamp": int64(0),
+			"dataset":   "cairo",
+			"delete": map[string]interface{}{
+				"foo": map[string]interface{}{
+					"bar": map[string]interface{}{
+						"route": map[string]interface{}{
+							"237.255.255.250_0.0.0.0": map[string]interface{}{},
+							"238.255.255.250_0.0.0.0": map[string]interface{}{},
+						},
+					},
+				},
 			},
-		}},
-	}
-	expected := map[string]interface{}{
-		"timestamp": int64(0),
-		"dataset":   "cairo",
-		"delete": map[string]interface{}{
-			"Smash": map[string]interface{}{
-				"routing": map[string]interface{}{
-					"pim": map[string]interface{}{
-						"sparsemode": map[string]interface{}{
-							"status": map[string]interface{}{
-								"default": map[string]interface{}{
-									"route": map[string]interface{}{
-										"237.255.255.250_0.0.0.0": map[string]interface{}{},
-										"238.255.255.250_0.0.0.0": map[string]interface{}{},
-									},
-								},
+			"update": map[string]interface{}{
+				"foo": map[string]interface{}{
+					"bar": map[string]interface{}{
+						"route": map[string]interface{}{
+							"239.255.255.250_0.0.0.0": map[string]interface{}{
+								"creationTime": 4.567969230573434e+06,
 							},
 						},
 					},
 				},
 			},
 		},
-		"update": map[string]interface{}{
-			"Smash": map[string]interface{}{
-				"routing": map[string]interface{}{
-					"pim": map[string]interface{}{
-						"sparsemode": map[string]interface{}{
-							"status": map[string]interface{}{
-								"default": map[string]interface{}{
-									"route": map[string]interface{}{
-										"239.255.255.250_0.0.0.0": map[string]interface{}{
-											"creationTime": 4.567969230573434e+06,
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+	},
 	}
-	actual, err := NotificationToMap("cairo", &notification, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	diff := test.Diff(expected, actual)
-	if len(diff) > 0 {
-		expectedJSON, _ := json.Marshal(expected)
-		actualJSON, _ := json.Marshal(actual)
-		t.Fatalf("Unexpected diff: %s\nExpected:\n%s\nGot:\n%s\n)", diff, expectedJSON,
-			actualJSON)
+	for _, tcase := range tests {
+		actual, err := NotificationToMap("cairo", &tcase.notification, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		diff := test.Diff(tcase.json, actual)
+		if len(diff) > 0 {
+			expectedJSON, _ := json.Marshal(tcase.json)
+			actualJSON, _ := json.Marshal(actual)
+			t.Fatalf("Unexpected diff: %s\nExpected:\n%s\nGot:\n%s\n)", diff, expectedJSON,
+				actualJSON)
+		}
 	}
 }
