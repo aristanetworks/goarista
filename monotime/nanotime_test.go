a37c8fb21e06e08098a521e4397c9b45e1d894b4
Kevin Burke <kev@inburke.com>
2017-01-02 09:14:31 -0800

    monotime: add Since function
    This is a convenience function for computing the elapsed time since a given
    timestamp, as it's a little cumbersome to type
    
    `time.Duration(monotime.Now() - start)`
    
    over and over. Adds an example to show how it should be used.
    
    This adds a dependency on the time package, which might not be desirable.

--- monotime/nanotime_test.go
+++ monotime/nanotime_test.go
@@ -7,6 +7,7 @@ package monotime_test
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/aristanetworks/goarista/monotime"
 )
@@ -19,6 +20,21 @@ func TestNow(t *testing.T) {
 		// two consecutive calls can return the same value!
 		if t1 > t2 {
 			t.Fatalf("t1=%d should have been less than or equal to t2=%d", t1, t2)
+		}
+	}
+}
+
+func TestSince(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		t1 := Now()
+		time.Sleep(1)
+		dur := Since(t1)
+		if dur <= 0 {
+			t.Fatalf("dur=%v should have been greater than 0", dur)
+		}
+		// avg value here is 5 nanoseconds but let's be safe.
+		if dur >= 10*time.Millisecond {
+			t.Fatalf("dur=%v was too large", dur)
 		}
 	}
 }
