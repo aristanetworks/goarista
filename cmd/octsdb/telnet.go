abe33277a636c047cb32ccabd1935a95244b60d2
Benoit Sigoure <tsuna@arista.com>
2017-03-06 17:17:25 -0800

    cmd/octsdb: Finally implement the `-tsdb` flag.
    Stream data points using the lame telnet-style protocol.
    
    Change-Id: Iae5a85a084ed3cbeaef7fb6c765090c06c277e4c

--- cmd/octsdb/telnet.go
+++ cmd/octsdb/telnet.go
@@ -4,8 +4,16 @@
 
 package main
 
+import (
+	"bytes"
+	"net"
+
+	"github.com/aristanetworks/glog"
+)
+
 type telnetClient struct {
 	addr string
+	conn net.Conn
 }
 
 func newTelnetClient(addr string) OpenTSDBConn {
@@ -14,6 +22,38 @@ func newTelnetClient(addr string) OpenTSDBConn {
 	}
 }
 
+func readErrors(conn net.Conn) {
+	var buf [4096]byte
+	for {
+		// TODO: We should add a buffer to read line-by-line properly instead
+		// of using a fixed-size buffer and splitting on newlines manually.
+		n, err := conn.Read(buf[:])
+		if n == 0 {
+			return
+		} else if n > 0 {
+			for line := range bytes.Split(buf[:n], []byte{'\n'}) {
+				glog.Info("tsd replied: ", string(line))
+			}
+		}
+		if err != nil {
+			return
+		}
+	}
+}
+
 func (c *telnetClient) Put(d *DataPoint) error {
-	panic("TODO")
+	var err error
+	if c.conn == nil {
+		c.conn, err = net.Dial("tcp", c.addr)
+		if err != nil {
+			return err
+		}
+		go readErrors(c.conn)
+	}
+	_, err = c.conn.Write([]byte(d.String()))
+	if err != nil {
+		c.conn.Close()
+		c.conn = nil
+	}
+	return err
 }
