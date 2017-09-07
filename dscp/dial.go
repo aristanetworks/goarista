7962203fdd729d79463e4afe86a2fa7376f2e77f
Giuseppe Valente <giuseppe@elastic.co>
2017-09-07 17:58:38 +0200

    dscp: add DialTcpWithTOS()
    We already have a way to set IP_TOS for a TCP server, this adds the same
    capability for TCP clients.
    
    Change-Id: I3efe5b50976719e2d8afbabdae24ea24159b9de5

--- dscp/dial.go
+++ dscp/dial.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2016  Arista Networks, Inc.
+// Copyright (C) 2017  Arista Networks, Inc.
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the COPYING file.
 
@@ -10,18 +10,18 @@ import (
 	"reflect"
 )
 
-// ListenTCPWithTOS is similar to net.ListenTCP but with the socket configured
+// DialTCPWithTOS is similar to net.DialTCP but with the socket configured
 // to the use the given ToS (Type of Service), to specify DSCP / ECN / class
 // of service flags to use for incoming connections.
-func ListenTCPWithTOS(address *net.TCPAddr, tos byte) (*net.TCPListener, error) {
-	lsnr, err := net.ListenTCP("tcp", address)
+func DialTCPWithTOS(laddr, raddr *net.TCPAddr, tos byte) (*net.TCPConn, error) {
+	conn, err := net.DialTCP("tcp", laddr, raddr)
 	if err != nil {
 		return nil, err
 	}
-	value := reflect.ValueOf(lsnr)
-	if err = setTOS(address, value, tos); err != nil {
-		lsnr.Close()
+	value := reflect.ValueOf(conn)
+	if err = setTOS(raddr, value, tos); err != nil {
+		conn.Close()
 		return nil, err
 	}
-	return lsnr, err
+	return conn, err
 }
