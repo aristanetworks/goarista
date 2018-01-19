71ea23d53efd5ae4ff6105555744a71bd708e1af
AJ Yoo <ajarusan@arista.com>
2018-01-19 11:37:58 -0800

    path: Wrap some lines and rephrase some comments
    Also, roughly group related methods together.
    
    Change-Id: Ib725f390a3ff75965a275c9c0ae2ca1bdade7058

--- path/path.go
+++ path/path.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the COPYING file.
 
-// Package path provides functionality for dealing with paths elementally.
+// Package path contains methods for dealing with absolute
+// paths elementally.
 package path
 
 import (
@@ -13,8 +14,30 @@ import (
 	"github.com/aristanetworks/goarista/key"
 )
 
-// Path is a path broken down into elements where each element is a key.Key.
+// Path represents an absolute path decomposed into elements
+// where each element is a key.Key.
 type Path []key.Key
+
+// New constructs a Path from a variable number of elements.
+// Each element may either be a string or a key.Key.
+func New(elements ...interface{}) Path {
+	path := make(Path, len(elements))
+	copyElements(path, elements...)
+	return path
+}
+
+// Append appends a variable number of elements to a Path.
+// Each element may either be a string or a key.Key.
+func Append(path Path, elements ...interface{}) Path {
+	if len(elements) == 0 {
+		return path
+	}
+	n := len(path)
+	p := make(Path, n+len(elements))
+	copy(p, path)
+	copyElements(p[n:], elements...)
+	return p
+}
 
 func copyElements(path Path, elements ...interface{}) {
 	for i, element := range elements {
@@ -29,12 +52,48 @@ func copyElements(path Path, elements ...interface{}) {
 	}
 }
 
-// New constructs a Path from a variable number of elements.
-// Each element may either be a string or a key.Key.
-func New(elements ...interface{}) Path {
-	path := make(Path, len(elements))
-	copyElements(path, elements...)
-	return path
+// String returns the Path as a string.
+func (p Path) String() string {
+	if len(p) == 0 {
+		return "/"
+	}
+	var buf bytes.Buffer
+	for _, element := range p {
+		buf.WriteByte('/')
+		buf.WriteString(element.String())
+	}
+	return buf.String()
+}
+
+// Equal returns whether the Path contains the same elements
+// as the other Path. This method implements key.Comparable.
+func (p Path) Equal(other interface{}) bool {
+	o, ok := other.(Path)
+	if !ok {
+		return false
+	}
+	if len(o) != len(p) {
+		return false
+	}
+	return o.hasPrefix(p)
+}
+
+// HasPrefix returns whether the Path is prefixed by the
+// other Path.
+func (p Path) HasPrefix(prefix Path) bool {
+	if len(prefix) > len(p) {
+		return false
+	}
+	return p.hasPrefix(prefix)
+}
+
+func (p Path) hasPrefix(prefix Path) bool {
+	for i := range prefix {
+		if !prefix[i].Equal(p[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 // FromString constructs a Path from the elements resulting
@@ -52,60 +111,4 @@ func FromString(str string) Path {
 		path[i] = key.New(element)
 	}
 	return path
-}
-
-// Append appends a variable number of elements to a Path.
-// Each element may either be a string or a key.Key.
-func Append(path Path, elements ...interface{}) Path {
-	if len(elements) == 0 {
-		return path
-	}
-	n := len(path)
-	p := make(Path, n+len(elements))
-	copy(p, path)
-	copyElements(p[n:], elements...)
-	return p
-}
-
-// String returns the Path as a string.
-func (p Path) String() string {
-	if len(p) == 0 {
-		return "/"
-	}
-	var buf bytes.Buffer
-	for _, element := range p {
-		buf.WriteByte('/')
-		buf.WriteString(element.String())
-	}
-	return buf.String()
-}
-
-// Equal returns whether the Path contains the same elements as the other Path.
-// This method implements key.Comparable.
-func (p Path) Equal(other interface{}) bool {
-	o, ok := other.(Path)
-	if !ok {
-		return false
-	}
-	if len(o) != len(p) {
-		return false
-	}
-	return o.hasPrefix(p)
-}
-
-// HasPrefix returns whether the Path is prefixed by the other Path.
-func (p Path) HasPrefix(prefix Path) bool {
-	if len(prefix) > len(p) {
-		return false
-	}
-	return p.hasPrefix(prefix)
-}
-
-func (p Path) hasPrefix(prefix Path) bool {
-	for i := range prefix {
-		if !prefix[i].Equal(p[i]) {
-			return false
-		}
-	}
-	return true
 }
