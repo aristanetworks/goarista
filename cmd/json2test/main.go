864e4d61868c4617735ad03daa4a3cb801d9c424
Aaron Beitch <aaronb@arista.com>
2018-03-15 15:08:16 -0700

    cmd/json2test: Print failures at the end
    Reprint test failure output at the end of the test log.
    
    Example:
    
    Test failures:
    [1] foo/bar.TestFoobar
    === RUN   TestFoobar
    --- FAIL: TestFoobar (0.13s)
    	foobar_test.go:82: mapper was not set
    
    [2] foo/baz.TestManager
    === RUN   TestManager
    SIGQUIT: quit
    <goroutine stack trace>
    *** Test killed with quit: ran too long (10m0s).
    FAIL	foo/baz	600.029s
    
    Change-Id: Ieaa3c24bc43414210da9a2a614f8cca6a4faa00e

--- cmd/json2test/main.go
+++ cmd/json2test/main.go
@@ -16,10 +16,24 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"log"
 	"os"
 	"time"
 )
+
+var errTestFailure = errors.New("testfailure")
+
+func main() {
+	err := writeTestOutput(os.Stdin, os.Stdout)
+	if err == errTestFailure {
+		os.Exit(1)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type testEvent struct {
 	Time    time.Time // encodes as an RFC3339-format string
@@ -30,11 +44,27 @@ type testEvent struct {
 	Output  string
 }
 
-func main() {
-	writeTestOutput(os.Stdin, os.Stdout)
+type test struct {
+	pkg  string
+	test string
+}
+
+type outputBuffer struct {
+	output []string
+}
+
+func (o *outputBuffer) push(s string) {
+	o.output = append(o.output, s)
+}
+
+type testFailure struct {
+	t test
+	o outputBuffer
 }
 
 func writeTestOutput(in io.Reader, out io.Writer) error {
+	testOutputBuffer := map[test]*outputBuffer{}
+	var failures []testFailure
 	d := json.NewDecoder(in)
 
 	buf := bufio.NewWriter(out)
@@ -44,15 +74,47 @@ func writeTestOutput(in io.Reader, out io.Writer) error {
 		if err := d.Decode(&e); err != nil {
 			break
 		}
+
 		switch e.Action {
 		default:
 			continue
+		case "run":
+			testOutputBuffer[test{pkg: e.Package, test: e.Test}] = new(outputBuffer)
+		case "pass":
+			// Don't hold onto text for passing
+			delete(testOutputBuffer, test{pkg: e.Package, test: e.Test})
+		case "fail":
+			// fail may be for a package, which won't have an entry in
+			// testOutputBuffer because packages don't have a "run"
+			// action.
+			t := test{pkg: e.Package, test: e.Test}
+			if o, ok := testOutputBuffer[t]; ok {
+				f := testFailure{t: t, o: *o}
+				delete(testOutputBuffer, t)
+				failures = append(failures, f)
+			}
 		case "output":
-		}
-
-		if _, err := buf.WriteString(e.Output); err != nil {
-			return err
+			buf.WriteString(e.Output)
+			// output may be for a package, which won't have an entry
+			// in testOutputBuffer because packages don't have a "run"
+			// action.
+			if o, ok := testOutputBuffer[test{pkg: e.Package, test: e.Test}]; ok {
+				o.push(e.Output)
+			}
 		}
 	}
-	return nil
+	if len(failures) == 0 {
+		return nil
+	}
+	buf.WriteString("\nTest failures:\n")
+	for i, f := range failures {
+		fmt.Fprintf(buf, "[%d] %s.%s\n", i+1, f.t.pkg, f.t.test)
+		for _, s := range f.o.output {
+			buf.WriteString(s)
+		}
+		if i < len(failures)-1 {
+			buf.WriteByte('\n')
+		}
+	}
+	return errTestFailure
 }
