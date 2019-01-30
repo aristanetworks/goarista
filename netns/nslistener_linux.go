be0489a019a4de78a7f82bcd2ad7c24812a21f83
Vlad Hanciuta <vlad@arista.com>
2019-01-30 17:20:48 +0000

    netns: Add a namespace listener
    The namespace listener will watch the named namespace and it tear down
    and recreate the underlying listener when the named namespace gets
    recreated.
    
    Change-Id: I476a6c834e0602b4e9a4a47d3ca02d3dbe218f17

---
+++ netns/nslistener_linux.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2019 Arista Networks, Inc.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the COPYING file.
+
+package netns
+
+import (
+	"errors"
+	"net"
+	"os"
+
+	"github.com/aristanetworks/glog"
+)
+
+var hasMount = func(mountPoint string) bool {
+	fd, err := os.Open("/proc/mounts")
+	if err != nil {
+		glog.Fatalf("can't open /proc/mounts")
+	}
+	defer fd.Close()
+
+	return hasMountInProcMounts(fd, mountPoint)
+}
+
+func getNsDir() (string, error) {
+	fd, err := os.Open("/proc/mounts")
+	if err != nil {
+		return "", errors.New("can't open /proc/mounts")
+	}
+	defer fd.Close()
+
+	return getNsDirFromProcMounts(fd)
+}
+
+// NewNSListener creates a new net.Listener bound to a network namespace. The listening socket will
+// be bound to the specified local address and will have the specified tos.
+func NewNSListener(nsName string, addr *net.TCPAddr, tos byte) (net.Listener, error) {
+	nsDir, err := getNsDir()
+	if err != nil {
+		return nil, err
+	}
+
+	return newNSListenerWithDir(nsDir, nsName, addr, tos)
+}
