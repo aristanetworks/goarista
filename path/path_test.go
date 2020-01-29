aristanetworks/goarista

commit 841b2df0a347c73492ed6a7dfcd04d6f1d628ac4

path: Let path.New supports []string []key.Key key.Path []key.Path
Let's say we have `A := []string{"foo", "bar"}`,
and we want to get `path.New("foo", "bar")`

Previously path.New(A...) wouldn't work
Now path.New(A) would return path.New("foo, "bar")
And path.New(A, []string{"baz", "foo"}) would return
path.New("foo", "bar", "baz", "foo")

Composite key is not affected. Other type of slices such as []int are not
supported

Change-Id: Idd0781a7229544b242e6dfe37c05da308edd0de6

path/path.go
@@ -15,9 +15,7 @@ import (
 // Each element may either be a key.Key or a value that can
 // be wrapped by a key.Key.
 func New(elements ...interface{}) key.Path {
-	result := make(key.Path, len(elements))
-	copyElements(result, elements...)
-	return result
+	return appendElements(nil, elements...)
 }
 
 // Append appends a variable number of elements to a path.
@@ -26,14 +24,7 @@ func New(elements ...interface{}) key.Path {
 // single path returns that same path, whereas in all other
 // cases a new path is returned.
 func Append(path key.Path, elements ...interface{}) key.Path {
-	if len(elements) == 0 {
-		return path
-	}
-	n := len(path)
-	result := make(key.Path, n+len(elements))
-	copy(result, path)
-	copyElements(result[n:], elements...)
-	return result
+	return appendElements(path, elements...)
 }
 
 // Join joins a variable number of paths together. Each path
@@ -140,15 +131,28 @@ func FromString(str string) key.Path {
 	return result
 }
 
-func copyElements(dest key.Path, elements ...interface{}) {
-	for i, element := range elements {
+func appendElements(dest key.Path, elements ...interface{}) key.Path {
+	for _, element := range elements {
 		switch val := element.(type) {
 		case key.Key:
-			dest[i] = val
+			dest = append(dest, val)
+		case []key.Key:
+			dest = append(dest, val...)
+		case key.Path:
+			dest = append(dest, val...)
+		case []string:
+			for i := range val {
+				dest = append(dest, key.New(val[i]))
+			}
+		case []key.Path:
+			for i := range val {
+				dest = append(dest, val[i]...)
+			}
 		default:
-			dest[i] = key.New(val)
+			dest = append(dest, key.New(val))
 		}
 	}
+	return dest
 }
 
 func hasPrefix(a, b key.Path) bool {

path/path_test.go
@@ -40,6 +40,24 @@ func TestNew(t *testing.T) {
 		}, {
 			in:  []interface{}{customKey{i: &a}, map[string]interface{}{}},
 			out: key.Path{key.New(customKey{i: &a}), key.New(map[string]interface{}{})},
+		}, {
+			in:  []interface{}{[]string{"A", "B"}, []string{"C"}, "D"},
+			out: key.Path{key.New("A"), key.New("B"), key.New("C"), key.New("D")},
+		}, {
+			in: []interface{}{[]string{"A", "B"}, []interface{}{"1", "2"}, "D"},
+			out: key.Path{key.New("A"), key.New("B"), key.New([]interface{}{"1", "2"}),
+				key.New("D")},
+		}, {
+			in: []interface{}{[]string{"A", "B"},
+				[]key.Key{key.New("C"), key.New("D")},
+				key.Path{key.New("E"), key.New("F")},
+				[]key.Path{
+					key.Path{key.New("G"), key.New("H")},
+					key.Path{key.New("I"), key.New("J")},
+				}},
+			out: key.Path{key.New("A"), key.New("B"), key.New("C"), key.New("D"),
+				key.New("E"), key.New("F"), key.New("G"), key.New("H"),
+				key.New("I"), key.New("J")},
 		},
 	}
 	for i, tcase := range tcases {

commit 4481d91782e56cf8870697d08897fa7c4aec1e2a

path: Ensure that Append makes a copy
commit 841b2df broke the expected behavior of Append making a copy of
destination.

Change-Id: I0051cce97d4b20957a89dcfb482513a53ca5437e

path/path.go
@@ -131,7 +131,15 @@ func FromString(str string) key.Path {
 	return result
 }
 
+// appendElements makes a copy of dest when elements is non-empty and
+// then appends elements to the copy and returns it.
 func appendElements(dest key.Path, elements ...interface{}) key.Path {
+	if len(elements) == 0 {
+		return dest
+	}
+	clone := make(key.Path, len(dest), len(dest)+len(elements))
+	copy(clone, dest)
+	dest = clone
 	for _, element := range elements {
 		switch val := element.(type) {
 		case key.Key:

path/path_test.go
@@ -701,3 +701,14 @@ func BenchmarkHasElement(b *testing.B) {
 		})
 	}
 }
+
+func TestAppendShouldCopy(t *testing.T) {
+	p := New("a", "b")
+	p = Append(p, "c")
+	d := Append(p, "d")
+	e := Append(p, "e")
+
+	if Equal(d, e) {
+		t.Errorf("paths d and e should not be equal: %s", e)
+	}
+}