804d9f34bc3533dcf865adb98fc90fec47f21d10
Christopher Neilson <christopher@arista.com>
2022-02-16 13:47:27 -0800

    cmd/gnmi: Change compression flag to be disabled by default
    While it is nice to have the option to enable compression,
    enabling it comes with a performance penalty. It is probably
    better to make this an opt-in behaviour for users that care
    more about bandwidth constraints.
    
    Change-Id: Id12a6b0c82c15a4d16fec70c8222c72a6152dfa5

--- cmd/gnmi/main.go
+++ cmd/gnmi/main.go
@@ -50,7 +50,7 @@ func main() {
 	flag.StringVar(&cfg.KeyFile, "keyfile", "", "Path to client TLS private key file")
 	flag.StringVar(&cfg.Password, "password", "", "Password to authenticate with")
 	flag.StringVar(&cfg.Username, "username", "", "Username to authenticate with")
-	flag.StringVar(&cfg.Compression, "compression", "gzip", "Compression method. "+
+	flag.StringVar(&cfg.Compression, "compression", "", "Compression method. "+
 		`Supported options: "" and "gzip"`)
 	flag.BoolVar(&cfg.TLS, "tls", false, "Enable TLS")
 
