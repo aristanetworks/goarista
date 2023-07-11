1b0691852ee498dea00b218d2f352b77b8c840ed
Aaron Beitch <aaronb@arista.com>
2023-07-11 13:34:46 -0700

    cmd/octsdb: Update test to be more flexible on input
    Change-Id: I4ceb13dc10dc921a714b3aac36f6fd25a3bb329e

--- cmd/octsdb/main_test.go
+++ cmd/octsdb/main_test.go
@@ -15,38 +15,37 @@ import (
 func TestParseValue(t *testing.T) { // Because parsing JSON sucks.
 	var staticValueMap map[string]int64
 	staticValueMap = make(map[string]int64)
+	newJSONVal := func(v string) *pb.TypedValue {
+		return &pb.TypedValue{
+			Value: &pb.TypedValue_JsonVal{JsonVal: []byte(v)},
+		}
+	}
+
 	testcases := []struct {
-		input          string
+		input          *pb.TypedValue
 		staticValueMap map[string]int64
 		expected       interface{}
 	}{
-		{"42", staticValueMap, []interface{}{int64(42)}},
-		{"-42", staticValueMap, []interface{}{int64(-42)}},
-		{"42.42", staticValueMap, []interface{}{float64(42.42)}},
-		{"-42.42", staticValueMap, []interface{}{float64(-42.42)}},
-		{`"foo"`, staticValueMap, []interface{}(nil)},
-		{"9223372036854775807", staticValueMap, []interface{}{int64(math.MaxInt64)}},
-		{"-9223372036854775808", staticValueMap, []interface{}{int64(math.MinInt64)}},
-		{"9223372036854775808", staticValueMap, []interface{}{uint64(math.MaxInt64) + 1}},
-		{"[1,3,5,7,9]", staticValueMap,
+		{newJSONVal("42"), staticValueMap, []interface{}{int64(42)}},
+		{newJSONVal("-42"), staticValueMap, []interface{}{int64(-42)}},
+		{newJSONVal("42.42"), staticValueMap, []interface{}{float64(42.42)}},
+		{newJSONVal("-42.42"), staticValueMap, []interface{}{float64(-42.42)}},
+		{newJSONVal(`"foo"`), staticValueMap, []interface{}(nil)},
+		{newJSONVal("9223372036854775807"), staticValueMap, []interface{}{int64(math.MaxInt64)}},
+		{newJSONVal("-9223372036854775808"), staticValueMap, []interface{}{int64(math.MinInt64)}},
+		{newJSONVal("9223372036854775808"), staticValueMap,
+			[]interface{}{uint64(math.MaxInt64) + 1}},
+		{newJSONVal("[1,3,5,7,9]"), staticValueMap,
 			[]interface{}{int64(1), int64(3), int64(5), int64(7), int64(9)}},
-		{"[1,9223372036854775808,0,-9223372036854775808]", staticValueMap, []interface{}{
-			int64(1),
-			uint64(math.MaxInt64) + 1,
-			int64(0),
-			int64(math.MinInt64)},
-		},
-		{`[1,{"value":9},5,7,9]`, staticValueMap,
+		{newJSONVal("[1,9223372036854775808,0,-9223372036854775808]"), staticValueMap,
+			[]interface{}{int64(1), uint64(math.MaxInt64) + 1, int64(0), int64(math.MinInt64)}},
+		{newJSONVal(`[1,{"value":9},5,7,9]`), staticValueMap,
 			[]interface{}{int64(1), int64(9), int64(5), int64(7), int64(9)}},
-		{`"intfOperUp"`, map[string]int64{"intfOperUp": 1}, []interface{}{int64(1)}},
-		{`"default"`, map[string]int64{"default": 0}, []interface{}{int64(0)}},
+		{newJSONVal(`"intfOperUp"`), map[string]int64{"intfOperUp": 1}, []interface{}{int64(1)}},
+		{newJSONVal(`"default"`), map[string]int64{"default": 0}, []interface{}{int64(0)}},
 	}
 	for i, tcase := range testcases {
-		actual := parseValue(&pb.Update{
-			Val: &pb.TypedValue{
-				Value: &pb.TypedValue_JsonVal{JsonVal: []byte(tcase.input)},
-			},
-		}, tcase.staticValueMap)
+		actual := parseValue(&pb.Update{Val: tcase.input}, tcase.staticValueMap)
 		if d := test.Diff(tcase.expected, actual); d != "" {
 			t.Errorf("#%d: %s: %#v vs %#v", i, d, tcase.expected, actual)
 		}
